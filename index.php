4ccb44481a9d49ab19146912d8c10b8f7b09b172
Luciano dii Souza <lucianodiisouza@hotmail.com>
2019-08-20 10:22:02 -0300

    add: none. mod: login file take the place of index file. shadow-box on loginBox Class

--- index.php
+++ index.php
@@ -1,39 +1,63 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" href="img/favicon.ico" sizes="32x32" />
     <title>AVA - Ambiente Virtual de Aprendizagem</title>
-    <link rel="stylesheet" type="text/css" href="css/estilo.css" />
+    <link rel="stylesheet" href="css/estilo.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/tonsky/FiraCode@1.207/distr/fira_code.css">
 </head>
 
 <body>
-    <div id="mySidebar" class="sidebar">
-        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-        <a href="#">Link</a>
-        <a href="#">Link</a>
-        <a href="#">Link</a>
-        <a href="#">Link</a>
-    </div>
+    <div class="loginBox">
+        <div class="esquerda">
+            <div class="esquerdaConteudo">
+                <h1 class="tituloEsquerdo">The Code Lovers</h1>
+                <p class="subtituloEsquerdo">WEB & Mobile</p>
+                <hr class="titleHr">
+                <div class="item">
+                    <img src="img/cloud.png" alt="Cloud Computing" class="itemImg">
+                    <p>Seu conteúdo sempre disponível.</p>
+                </div>
+                <div class="item">
+                    <img src="img/mortarboard.png" alt="Academy Hat" class="itemImg">
+                    <p>Cursos com certificados.</p>
+                </div>
+                <div class="item">
+                    <img src="img/electronics.png" alt="Ideas Lamp" class="itemImg">
+                    <p>Formação completa gratuita.</p>
+                </div>
+            </div>
+            <div class="espaco">
 
-    <div id="main"  >
-        <button class="openbtn" onclick="openNav()">&#9776;</button>
-        <h2></h2>
-        <p><p>
+            </div>
+        </div>
+        <div class="direita">
+            <div class="titleContainer">
+            </div>
+            <div class="formContainer">
+                <p class="titleForm">Insira suas credenciais</p>
+                <form method="post" class="formulariologin" autocomplete="off">
+                    <input type="mail" name="usuario" placeholder="Seu email" class="inputLoginMail" required>
+                    <input type="password" name="senha" placeholder="Sua senha" class="inputLoginSenha" required>
+                    <button type="submit" class="btnLogin">Login</button>
+                </form>
+            </div>
+            <div>
+                <p class="linkContainer">
+                    <a href="#" class="footerLink">Não consigo fazer login.</a>
+                </p>
+                <hr class="footerHr">
+                <p class="linkContainer">
+                    <a href="#" class="footerLink">Não tenho cadastro.</a>
+                </p>
+            </div>
+        </div>
     </div>
-<script>
-    /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
-function openNav() {
-  document.getElementById("mySidebar").style.width = "200px";
-  document.getElementById("main").style.marginLeft = "200px";
-}
-
-/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
-function closeNav() {
-  document.getElementById("mySidebar").style.width = "0";
-  document.getElementById("main").style.marginLeft = "0";
-}
-</script>
 </body>
+
 </html>
