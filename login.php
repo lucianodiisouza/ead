c6f59c52447e3e94233e7e7e19a6695a20724560
Luciano dii Souza <lucianodiisouza@hotmail.com>
2019-08-20 05:09:27 -0300

    add: iconImages, CSS File. mod: index.php, login.php. 'last' 20/08 commit, (but it's the first of the day)

--- login.php
+++ login.php
@@ -1,24 +1,63 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>AVA - Login</title>
+    <link rel="icon" href="img/favicon.ico" sizes="32x32" />
+    <title>AVA - Ambiente Virtual de Aprendizagem</title>
     <link rel="stylesheet" href="css/estilo.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/tonsky/FiraCode@1.207/distr/fira_code.css">
 </head>
+
 <body>
     <div class="loginBox">
         <div class="esquerda">
+            <div class="esquerdaConteudo">
+                <h1 class="tituloEsquerdo">The Code Lovers</h1>
+                <p class="subtituloEsquerdo">WEB & Mobile</p>
+                <hr class="titleHr">
+                <div class="item">
+                    <img src="img/cloud.png" alt="Cloud Computing" class="itemImg">
+                    <p>Seu conteúdo sempre disponível.</p>
+                </div>
+                <div class="item">
+                    <img src="img/mortarboard.png" alt="Academy Hat" class="itemImg">
+                    <p>Cursos com certificados.</p>
+                </div>
+                <div class="item">
+                    <img src="img/electronics.png" alt="Ideas Lamp" class="itemImg">
+                    <p>Formação completa gratuita.</p>
+                </div>
+            </div>
+            <div class="espaco">
 
+            </div>
         </div>
         <div class="direita">
-            <form method="post" class="formulariologin">
-                <input type="text" name="usuario" placeholder="Seu email" class="inputLogin">
-                <input type="text" name="senha" placeholder="Sua senha" class="inputLogin">
-                <button type="submit">Login</button>
-            </form>
+            <div class="titleContainer">
+            </div>
+            <div class="formContainer">
+                <p class="titleForm">Insira suas credenciais</p>
+                <form method="post" class="formulariologin" autocomplete="off">
+                    <input type="text" name="usuario" placeholder="Seu email" class="inputLoginMail">
+                    <input type="text" name="senha" placeholder="Sua senha" class="inputLoginSenha">
+                    <button type="submit" class="btnLogin">Login</button>
+                </form>
+            </div>
+            <div>
+                <p class="linkContainer">
+                    <a href="#" class="footerLink">Não consigo fazer login.</a>
+                </p>
+                <hr class="footerHr">
+                <p class="linkContainer">
+                    <a href="#" class="footerLink">Não tenho cadastro.</a>
+                </p>
+            </div>
         </div>
     </div>
 </body>
+
 </html>
